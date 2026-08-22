de2a843c945b62b5e619464e4f7e6d334a07a7ed
davidwoolery <sheurm@gmail.com>
2026-08-22 16:45:48 -0500

    Mirror the Documents column layout on the right sidebar
    Adds a matching empty spacer column between Plot Viewer and the right
    sidebar, and gives the sidebar the same smaller font-size and a
    padding-right capped at min(50px, 5%), mirroring the Documents
    column's padding-left. Plot Viewer gives up one grid unit to make
    room.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -651,7 +651,7 @@ function AppContent() {
 
         <div className="d-none d-lg-block col-lg-1 order-lg-2" />
 
-        <div className="col-12 col-lg-7 order-1 order-lg-3 d-flex flex-column gap-4">
+        <div className="col-12 col-lg-6 order-1 order-lg-3 d-flex flex-column gap-4">
             <div className="card">
               <div className="card-header">
                 <h2 className="h5 mb-0">Plot Viewer</h2>
@@ -744,7 +744,9 @@ function AppContent() {
             </div>
         </div>
 
-        <div className="col-12 col-lg-2 order-3 order-lg-4 d-flex flex-column gap-4">
+        <div className="d-none d-lg-block col-lg-1 order-lg-4" />
+
+        <div className="col-12 col-lg-2 order-3 order-lg-5 d-flex flex-column gap-4 sidebar-panel">
           <div className="card">
             <div className="card-header">
               <h2 className="h5 mb-0">Encoding Rules</h2>
